3bbb039848b8ec0eda7e955696c82d2faa213994
Boris Krasnov <crasnov.boris@yandex.ru>
2023-11-13 19:23:20 +0400

    работа с зашедшим клиентом

--- cmd/web/manager.go
+++ cmd/web/manager.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 var (
@@ -16,12 +18,20 @@ var (
 )
 
 type Manager struct {
+	clients ClientList
+	sync.RWMutex 
 }
+
+
 
 func NewManager() *Manager {
-	return &Manager{}
+	return &Manager{
+		clients: make(ClientList),
+
+	}
 }
 
+// Подключение к вебсокету 
 func (m *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
 	log.Println("New connection")
 
@@ -33,5 +43,34 @@ func (m *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	conn.Close()
+	// Добавление нового клиента при соединении
+	client := NewClient(conn,m)
+	
+	// добавление новвого клиента в слайс который является свойством типа Manager
+	m.addClient(client)
 }
+
+// Добавление клиента в менеджер
+func (m *Manager) addClient(client *Client){
+	// Если идет одновременно два соединения , блокирует одно из них 
+	m.Lock()
+	
+	// Пропускает его и разблокирует обратно 
+	defer m.Unlock()
+
+	// добавление зашедшего клиента в мапу
+	m.clients[client] = true
+}
+
+// Удаление клиента 
+func (m *Manager) removeClient(client *Client){
+	m.Lock()
+	defer m.Unlock()
+
+	if _,ok := m.clients[client]; ok {
+		
+		client.connection.Close()
+		delete(m.clients,client)
+	}
+}
+
